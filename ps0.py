38fc3ccc490dbc8a1692342beb6092166cf5fe9b
Cyrus <gautam.cyrus80@gmail.com>
2021-02-09 19:21:48 -0800

    Create ps0.py

---
+++ ps0.py
@@ -0,0 +1,12 @@
+# Importing necessary library
+import math
+
+# The value of x and y. These two lines asks the value of x and y from the user. 
+# int changes the value of x from string to integer
+x = int(input('Enter a value: x = '))
+y = int(input('Enter a value: y = '))
+
+# Prints x power y 
+print("x^y = ",x**y)
+# Prints the log with base 2 of x.
+print("log2(2) = ",math.log2(x))
